e5f8594a15ede366b1e218103aa6e23895357bd3
Gianmarco720 <orabonagianmarco379@gmail.com>
2022-10-12 11:39:08 +0200

    end jsnack1

---
+++ jsnack1/script.js
@@ -0,0 +1,19 @@
+/* 
+Snack 1
+L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore. 
+*/ 
+
+// chiedo all' utente di inserire due numeri
+const firstNum = Number(prompt('Inserisci il primo numero:'));
+const secondNum = Number(prompt('Inserisci il secondo numero:'));
+
+// stampo il risultato con un if statement
+let result;
+if (firstNum > secondNum) {
+    result = firstNum;
+    console.log(`Il numero più grande è: ${result}`);
+} else if (secondNum > firstNum) {
+    result = secondNum;
+    console.log(`Il numero più grande è: ${result}`);
+}
