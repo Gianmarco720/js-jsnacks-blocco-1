8c2ce61f9e59b17d360d4ed43d92e21ff8ec23c6
Gianmarco720 <orabonagianmarco379@gmail.com>
2022-10-12 12:49:05 +0200

    add comments

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -4,11 +4,12 @@ Il software deve chiedere per 10 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti
 */
 
-// chiedo all'utente i numeri da inserire
+// chiedo all'utente i numeri da inserire con un ciclo for
 let somma = 0;
 for (let i = 1; i <= 10; i++) {
     const userNum = Number(prompt(`Inserisci il ${i} numero:`));
+    // calcolo la somma di tutti i numeri inseriti
      somma = somma + userNum;
 }
-
-console.log('La somma è', somma);
+// stampo la somma in console
+console.log('La somma è:', somma);
