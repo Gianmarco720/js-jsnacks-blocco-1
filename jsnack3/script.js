867dda68797223e7ef4a5854bc4698cb38fd36ef
Gianmarco720 <orabonagianmarco379@gmail.com>
2022-10-13 12:02:58 +0200

    add while loop

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -5,11 +5,23 @@ Il programma stampa la somma di tutti i numeri inseriti
 */
 
 // chiedo all'utente i numeri da inserire con un ciclo for
-let somma = 0;
+/* let somma = 0;
 for (let i = 1; i <= 10; i++) {
     const userNum = Number(prompt(`Inserisci il ${i} numero:`));
     // calcolo la somma di tutti i numeri inseriti
      somma = somma + userNum;
 }
 // stampo la somma in console
-console.log('La somma è:', somma);
+console.log('La somma è:', somma); */
+
+// chiedo all' utente i numeri da inserire ma con un ciclo while
+let somma = 0;
+let i = 0;
+
+while (i < 10) {
+    i++
+    const userNum = Number(prompt(`Inserisci un numero:`));
+    somma = somma + userNum;
+}
+
+console.log(somma);
